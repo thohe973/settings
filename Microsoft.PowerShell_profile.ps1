f0dd5c72dc375ed4942e610a55de419b5638a05c
Thomas Henriksson <thohe973@student.liu.se>
2020-05-15 10:06:42 +0200

    powershell

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,44 @@
+function Write-BranchName () {
+  try {
+    $branch = git symbolic-ref --short -q HEAD
+
+    if ($branch -ne $null) {
+      Write-Host " ($branch)" -ForegroundColor "cyan" -NoNewline
+    }
+    else {
+      # Detached state
+      $branch = git rev-parse --short HEAD
+      Write-Host " ($branch)" -ForegroundColor "red" -NoNewline
+    }
+  } catch {
+    Write-Host " (unkown)" -ForegroundColor "yellow" -NoNewline
+  }
+}
+
+# Add git branch to prompt
+function prompt {
+  $base = ""
+  $path = "$($executionContext.SessionState.Path.CurrentLocation)"
+
+  if (Test-Path .git) {
+    Write-Host $path -NoNewline -ForegroundColor "blue"
+    Write-BranchName
+    Write-Host ">" -NoNewline -ForegroundColor "blue"
+  }
+  else {
+    Write-Host $path -NoNewline -ForegroundColor "blue"
+    Write-Host ">" -NoNewline -ForegroundColor "blue"
+  }
+
+  return " "
+}
+
+# Autocomplete 'cd ..' to 'cd ..\' when tabbing
+Copy Function:TabExpansion2 Function:OriginalTabExpansion
+function TabExpansion([string] $line, [string] $lastword) {
+  if ($line -eq 'cd ..') {
+    return '..\'
+  } else {
+   OriginalTabExpansion $line $lastword | Out-Host
+  }
+}
